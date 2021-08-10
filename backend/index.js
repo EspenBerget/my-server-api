04a43bb9c9ef6d7bd062a13d06c89ba41ba7eb25
Espen Berget <espen.berget@tuta.io>
2021-08-10 17:32:49 +0200

    added a sqlite db and a simple db api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
-// import { open, save } from "<https://deno.land/x/sqlite/mod.ts>";
 import { Server } from './server.js';
+import database, { removeEvent } from './db.js';
 
 const server = new Server(8001);
 
 server.get('/', req => {
     req.respond({body: "hello world"});
 });
+
+server.get('/request', req => {
+    req.respond({body: JSON.stringify(req)});
+})
+
+server.get('/all', req => {
+    const events = database.getAllEvents();
+    req.respond({body: JSON.stringify(events)});
+})
+
+//server.get("/events/:id", TODO);
 
 await server.listen();
