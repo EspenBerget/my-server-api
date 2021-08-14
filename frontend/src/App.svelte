9bb619032d347dcba1fbb49e9b110c532b4b109e
Espen Berget <espen.berget@tuta.io>
2021-08-14 14:00:49 +0200

    made a simple frontend, displays all events

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,6 +1,35 @@
+<script>
+	import { onMount } from 'svelte';
+	import Event from './Event.svelte';
+
+
+	let events = [];
+	let error;
+
+	onMount(async () => {
+		const response = await fetch('http://localhost:8001/events');
+		if (response.ok) {
+			events = await response.json();
+		} else {
+			error = response.status;
+		}
+	});
+
+
+</script>
+
 <main>
-	<h1> TODO </h1>
+	{#each events as event (event[0]) }
+	<Event id={event[0]} desc={event[1]} timestamp={event[2]} />
+	{/each}
+
+	{#if error}
+	<p class="error">{error}</p>
+	{/if}
 </main>
 
 <style>
+	.error {
+		color: red;
+	}
 </style>
---
+++ frontend/src/Event.svelte
@@ -0,0 +1,41 @@
+<script>
+    export let id;
+    export let desc;
+    export let timestamp;
+</script>
+
+
+<article class="event">
+    <span>Ref: {id}</span>
+    <p class="text">{desc}</p>
+    <span class="timestamp">{timestamp}</span>
+</article>
+
+<style>
+    .event {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        min-height: 5rem;
+
+        border: 1px solid black;
+        
+        margin: 0.5rem;
+
+        padding-left: 0.3rem;
+        padding-right: 0.3rem;
+        padding-top: 0.1rem;
+        padding-bottom: 0.1rem;
+    }
+
+    .text {
+        font-size: 1.5rem;
+    }
+
+
+    .timestamp {
+        font-family: monospace;
+        align-self: flex-start;
+    }
+</style>
